a2b7757dd1b51ab375c1f96f1a25f68f7ebbef2c
Nidal <nidal@level09.com>
2014-08-19 02:20:23 +0400

    added protection to flask admin

--- admin/views.py
+++ admin/views.py
@@ -1,12 +1,17 @@
 from flask_admin.contrib.mongoengine import ModelView
 from user.models import User
+from flask_security import current_user
+from flask_admin import expose, BaseView
+
 
 class UserView(ModelView):
-    
     column_list = ['created_at', 'email','active','confirmed_at','last_login_at','roles']
     form_columns = ['email','roles']
+    def is_accessible(self):
+        return current_user.has_role('admin')
 
 
 
 class RoleView(ModelView):
-	pass
+    def is_accessible(self):
+        return current_user.has_role('admin')
