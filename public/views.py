abfead9baa2e031e2601c0e9f72e023c205100ce
Nidal <nidal@level09.com>
2014-12-24 17:33:01 +0400

    added robots.txt

--- public/views.py
+++ public/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, Response, redirect, url_for, flash, Blueprint
+from flask import Flask, request, abort, Response, redirect, url_for, flash, Blueprint, send_from_directory
 from flask.templating import render_template
 from flask_security.decorators import roles_required, login_required
 
@@ -9,3 +9,6 @@ def index():
     return render_template('index.html')
 
 
+@bp_public.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(bp_public.static_folder, request.path[1:])
