32aa842f63def6f6f351bca44f081fff8fc95733
Nidal <nidal@level09.com>
2020-06-09 02:13:19 +0200

    added compatibility for the new flask-security-too

--- enferno/commands.py
+++ enferno/commands.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 """Click commands."""
 import os
-from glob import glob
-from subprocess import call
 
 import click
-from flask import current_app
 from flask.cli import with_appcontext
-from werkzeug.exceptions import MethodNotAllowed, NotFound
+from flask_security.utils import hash_password
 
 from enferno.extensions import db
-from flask_security.script import CreateUserCommand, AddRoleCommand
-from flask_security.utils import encrypt_password
 from enferno.user.models import User
-from flask import current_app
-from flask.cli import with_appcontext
 
 
 @click.command()
@@ -22,9 +15,8 @@ from flask.cli import with_appcontext
 def create_db():
     """creates db tables - import your models within commands.py to create the models.
     """
-    # check if admin exists
     db.create_all()
-    print ('Database structure created successfully')
+    print('Database structure created successfully')
 
 
 @click.command()
@@ -36,19 +28,20 @@ def install():
     from enferno.user.models import Role
     a = Role.query.filter(Role.name == 'admin').first()
 
-    if a == None:
+    if a is None:
         r = Role(name='admin')
         try:
             db.session.add(r)
             db.session.commit()
             u = click.prompt('Admin Email?', default='admin@enferno.io')
             p = click.prompt('Admin Password (min 6 characters)?', default='enferno')
-            CreateUserCommand().run(email=u, password=p, active=1)
-            AddRoleCommand().run(user_identifier=u, role_name='admin')
-        except:
+            user = User(email=u, password=hash_password(p), active=1)
+            user.roles.append(r)
+            user.save()
+        except Exception as e:
             db.session.rollback()
     else:
-        print ('Seems like an Admin is already installed')
+        print('Seems like an Admin is already installed')
 
 
 @click.command()
@@ -61,10 +54,10 @@ def create(email, password):
 
     a = User.query.filter(User.email == email).first()
     if a != None:
-        print ('User already exists!')
+        print('User already exists!')
     else:
-        CreateUserCommand().run(email=email, password=password, active=1)
-        # you can add default roles here
+        user = User(email=email, password=hash_password(password), active=1)
+        user.save()
 
 
 @click.command()
@@ -76,44 +69,44 @@ def add_role(email, role):
         """
     from enferno.user.models import Role
     u = User.query.filter(User.email == email).first()
-    if u == None:
-        print ('Sorry, this user does not exist!')
-        return
-    r = Role.query.filter(Role.name == role).first()
-    if r == None:
-        print ('Sorry, this role does not exist!')
-        u = click.prompt('Would you like to create one? Y/N', default='N')
-        if u.lower() == 'y':
-            r = Role(name=role)
-            try:
-                db.session.add(r)
-                db.session.commit()
-                print ('Role created successfully, you may add it now to the user')
-            except:
-                db.session.rollback()
 
+    if u is None:
+        print('Sorry, this user does not exist!')
     else:
-        AddRoleCommand().run(user_identifier=email, role_name=role)
+        r = Role.query.filter(Role.name == role).first()
+        if r is None:
+            print('Sorry, this role does not exist!')
+            u = click.prompt('Would you like to create one? Y/N', default='N')
+            if u.lower() == 'y':
+                r = Role(name=role)
+                try:
+                    db.session.add(r)
+                    db.session.commit()
+                    print('Role created successfully, you may add it now to the user')
+                except Exception as e:
+                    db.session.rollback()
+        # add role to user
+        u.roles.append(r)
 
 
 @click.command()
 @click.option('-e', '--email', prompt=True, default=None)
-@click.option('-p', '--password',hide_input=True,confirmation_prompt=True, prompt=True, default=None)
+@click.option('-p', '--password', hide_input=True, confirmation_prompt=True, prompt=True, default=None)
 @with_appcontext
 def reset(email, password):
     """Reset a user password
     """
     try:
-        pwd = encrypt_password(password)
+        pwd = hash_password(password)
         u = User.query.filter(User.email == email).first()
         u.password = pwd
         try:
             db.session.commit()
-            print ('User password has been reset successfully.')
+            print('User password has been reset successfully.')
         except:
             db.session.rollback()
     except Exception as e:
-        print ('Error resetting user password: %s' % e)
+        print('Error resetting user password: %s' % e)
 
 
 @click.command()
