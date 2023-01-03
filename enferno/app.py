b28ac8879d3bffa8c59bf3107df98ab5179f93e3
Nidal <nidal@level09.com>
2023-01-03 23:55:35 +0100

    rolling a fix for a bug when registration confirmable settings is enabled

--- enferno/app.py
+++ enferno/app.py
@@ -5,13 +5,13 @@ from enferno.settings import ProdConfig
 from flask_security import Security, SQLAlchemyUserDatastore
 from enferno.user.models import User, Role
 from enferno.user.forms import ExtendedRegisterForm
-from enferno.extensions import   cache,  db,  mail, debug_toolbar, migrate, session
+from enferno.extensions import cache, db, mail, debug_toolbar, migrate, session
 from enferno.public.views import bp_public
 from enferno.user.views import bp_user
 import enferno.commands as commands
 
-def create_app(config_object=ProdConfig):
 
+def create_app(config_object=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config_object)
     register_extensions(app)
@@ -22,30 +22,30 @@ def create_app(config_object=ProdConfig):
     return app
 
 
-
 def register_extensions(app):
     cache.init_app(app)
     db.init_app(app)
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-    security = Security(app, user_datastore, register_form=ExtendedRegisterForm)
+    security = Security(app, user_datastore, confirm_register_form=ExtendedRegisterForm,
+                        register_form=ExtendedRegisterForm)
     mail.init_app(app)
     debug_toolbar.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
     session.init_app(app)
     return None
 
 
 def register_blueprints(app):
     app.register_blueprint(bp_public)
-    app.register_blueprint(bp_user)            
+    app.register_blueprint(bp_user)
     return None
-
 
 
 def register_errorhandlers(app):
     def render_error(error):
         error_code = getattr(error, 'code', 500)
         return render_template("{0}.html".format(error_code)), error_code
+
     for errcode in [401, 404, 500]:
         app.errorhandler(errcode)(render_error)
     return None
@@ -53,6 +53,7 @@ def register_errorhandlers(app):
 
 def register_shellcontext(app):
     """Register shell context objects."""
+
     def shell_context():
         """Shell context objects."""
         return {
@@ -64,7 +65,6 @@ def register_shellcontext(app):
     app.shell_context_processor(shell_context)
 
 
-
 def register_commands(app):
     """Register Click commands."""
 
@@ -74,4 +74,3 @@ def register_commands(app):
     app.cli.add_command(commands.create)
     app.cli.add_command(commands.add_role)
     app.cli.add_command(commands.reset)
-
