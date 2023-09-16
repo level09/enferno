49faa6a7514493d125ae454770ba4d78374fcc5c
Nidal <nidal@level09.com>
2023-09-17 00:14:04 +0200

    added initial vite configurations

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,23 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [vue()],
     base: '/static/dist/', // Setting the base URL for assets
-
     build: {
+
         rollupOptions: {
             input: {
-                index: './enferno/src/index.js'
+                index: resolve(__dirname, 'enferno/src/index.js'),
             },
             output: {
                 entryFileNames: `[name].js`,
-                chunkFileNames: `[name].js`,
                 assetFileNames: `[name].[ext]`,
-
-                manualChunks: undefined
             }
         },
         sourcemap: true,
         target: 'esnext',
-        outDir: './enferno/static/dist/'
+        outDir: resolve(__dirname, 'enferno/static/dist/')
     }
 })
